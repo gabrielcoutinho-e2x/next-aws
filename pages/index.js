765517ee4ad6dbf09254168fda34c39eba1d7a6f
Gabriel Fonseca <gabrielfonseca.coutinho@outlook.com>
2021-06-09 12:06:18 +0100

    integrating with amplify

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { DataStore } from "aws-amplify";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { Post } from "../src/models";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+    async function fetchPosts() {
+      const postData = await DataStore.query(Post);
+      setPosts(postData);
+    }
+
+    const subscription = DataStore.observe(Post).subscribe(() => fetchPosts());
+    return () => subscription.unsubscribe();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,6 +28,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>My Next.js Amplify app</h1>
+        <div className={styles.container}>
+          <h1>Posts</h1>
+          {posts.map((post) => (
+            <Link href={`/posts/${post.id}`}>
+              <a>
+                <h2>{post.title}</h2>
+              </a>
+            </Link>
+          ))}
+        </div>
       </main>
     </div>
   );
